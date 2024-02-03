d912a63bda6bc7dee33823ecf122beec8049b670
Kaushalya13 <nimanthikaushalya13@gmail.com>
2024-02-03 11:07:32 +0530

    update

---
+++ src/main/java/com/example/task/JSONBind.java
@@ -0,0 +1,31 @@
+package com.example.task;
+
+import jakarta.json.bind.Jsonb;
+import jakarta.json.bind.JsonbBuilder;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "task",urlPatterns = "/task")
+public class JSONBind extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String name = req.getParameter("name");
+        String city = req.getParameter("city");
+
+        System.out.println(name);
+        System.out.println(city);
+
+        if (req.getContentType() == null || ! req.getContentType().toLowerCase().startsWith("application/json")){
+            resp.sendError(HttpServletResponse.SC_UNSUPPORTED_MEDIA_TYPE);
+        }else {
+            Jsonb jsonb = JsonbBuilder.create();
+
+        }
+
+    }
+}
